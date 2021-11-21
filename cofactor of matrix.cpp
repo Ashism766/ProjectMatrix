53bdee6dddac435cc3688cf70499f076f40660ae
Ashis <maity.ashis651@gmail.com>
2021-11-21 14:13:34 +0530

    first commit

---
+++ cofactor of matrix.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int A[3][3];
+	for(int i=0;i<3;++i)
+	{
+		for(int j=0;j<3;++j)
+		{
+			cin>>A[i][j];
+		}
+	}
+	
+}
